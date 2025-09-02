f5b48da6db5820797beae661af7afb0b74ba30d2
ThomasReyel <thomasreyel2231@gmail.com>
2025-09-02 20:05:09 -0300

    adição dos exemplos de conexão de bd

---
+++ node-banco/config/db.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "2231",
+        database: "exemplo_db",
+      });
+      
+      
+      // Conecta ao banco de dados
+      connection.connect((err) => {
+        if (err) {
+          console.error("Erro ao conectar ao banco de dados:", err);
+          return;
+        }
+        console.log("Conectado ao banco de dados MySQL com sucesso!");
+});
+      
+module.exports = connection;
